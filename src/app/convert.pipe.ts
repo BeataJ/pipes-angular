7b1b8746964de0ac7969c7e9892202b19ae2bf22
beata <beata@emc2wave.com>
2020-12-29 15:43:27 -0800

    add switch

--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -4,10 +4,19 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'convert',
 })
 export class ConvertPipe implements PipeTransform {
-  transform(value: any, ...args: any[]): any {
+  transform(value: any, targetUnit: string): any {
     if (!value) {
       return '';
     }
-    return value * 1.609344;
+    switch (targetUnit) {
+      case 'km':
+        return value * 1.609344;
+      case 'm':
+        return value * 1.609344 * 1000;
+      case 'cm':
+        return value * 1.609344 * 1000 * 1000;
+      default:
+        throw new Error('Target unit not supported');
+    }
   }
 }
